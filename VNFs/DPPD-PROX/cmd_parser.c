opnfv/samplevnf

commit 843ca042cad997b51390bd6156d3200de18294d2

Add support for latency task with multiple ports
Change-Id: Iaae603a7558666b5794ec4c76f447eb3260a1df9
Signed-off-by: Xavier Simonart <xavier.simonart@intel.com>
Signed-off-by: Patrice Buriez <patrice.buriez@intel.com>

VNFs/DPPD-PROX/cmd_parser.c
@@ -1764,6 +1764,19 @@ static int parse_cmd_lat_stats(const char *str, struct input *input)
 			else {
 				struct stats_latency *stats = stats_latency_find(lcore_id, task_id);
 				struct stats_latency *tot = stats_latency_tot_find(lcore_id, task_id);
+				if (!stats || !tot) {
+					if (input->reply) {
+						char buf[128];
+						snprintf(buf, sizeof(buf),
+							 "error: core %u task %u stats = %p tot = %p\n",
+							 lcore_id, task_id, stats, tot);
+						input->reply(input, buf, strlen(buf));
+					} else {
+						plog_info("error: core %u task %u stats = %p tot = %p\n",
+							  lcore_id, task_id, stats, tot);
+					}
+					continue;
+				}
 
 				uint64_t last_tsc = stats_core_task_last_tsc(lcore_id, task_id);
 				uint64_t lat_min_usec = time_unit_to_usec(&stats->min.time);

VNFs/DPPD-PROX/stats_latency.c
@@ -104,11 +104,11 @@ struct stats_latency *stats_latency_find(uint32_t lcore_id, uint32_t task_id)
 
 static int task_runs_observable_latency(struct task_args *targ)
 {
-	/* TODO: make this work with multiple ports and with
-	   rings. Currently, only showing lat tasks which have 1 RX
-	   port. */
+	/* Note that multiple ports or rings are only supported
+	   if they all receive packets configured in the same way
+	   e.g. same timestamp pos. */
 	return !strcmp(targ->task_init->mode_str, "lat") &&
-		(targ->nb_rxports == 1 || targ->nb_rxrings == 1);
+		(targ->nb_rxports >= 1 || targ->nb_rxrings >= 1);
 }
 
 static struct stats_latency_manager *alloc_stats_latency_manager(void)

commit c8e9e6bb696363a397b2e718eb4d3e5f38a8ef22

Fix "code nested too deep error" introduced by commit 843ca042
Change-Id: Ifb55091feedb28ffc5947ffe61e6f6a5ce034459
Signed-off-by: Xavier Simonart <xavier.simonart@intel.com>

VNFs/DPPD-PROX/cmd_parser.c
@@ -1752,7 +1752,9 @@ static int parse_cmd_lat_stats(const char *str, struct input *input)
 				} else {
 					plog_info("error: invalid core %u, task %u\n", lcore_id, task_id);
 				}
-			} else if (!task_is_mode(lcore_id, task_id, "lat")) {
+				continue;
+			}
+			if (!task_is_mode(lcore_id, task_id, "lat")) {
 				if (input->reply) {
 					char buf[128];
 					snprintf(buf, sizeof(buf), "error: core %u task %u is not measuring latency\n", lcore_id, task_id);
@@ -1760,56 +1762,56 @@ static int parse_cmd_lat_stats(const char *str, struct input *input)
 				} else {
 					plog_info("error: core %u task %u is not measuring latency\n", lcore_id, task_id);
 				}
+				continue;
 			}
-			else {
-				struct stats_latency *stats = stats_latency_find(lcore_id, task_id);
-				struct stats_latency *tot = stats_latency_tot_find(lcore_id, task_id);
-				if (!stats || !tot) {
-					if (input->reply) {
-						char buf[128];
-						snprintf(buf, sizeof(buf),
-							 "error: core %u task %u stats = %p tot = %p\n",
-							 lcore_id, task_id, stats, tot);
-						input->reply(input, buf, strlen(buf));
-					} else {
-						plog_info("error: core %u task %u stats = %p tot = %p\n",
-							  lcore_id, task_id, stats, tot);
-					}
-					continue;
-				}
 
-				uint64_t last_tsc = stats_core_task_last_tsc(lcore_id, task_id);
-				uint64_t lat_min_usec = time_unit_to_usec(&stats->min.time);
-				uint64_t lat_max_usec = time_unit_to_usec(&stats->max.time);
-				uint64_t tot_lat_min_usec = time_unit_to_usec(&tot->min.time);
-				uint64_t tot_lat_max_usec = time_unit_to_usec(&tot->max.time);
-				uint64_t lat_avg_usec = time_unit_to_usec(&stats->avg.time);
-
+			struct stats_latency *stats = stats_latency_find(lcore_id, task_id);
+			struct stats_latency *tot = stats_latency_tot_find(lcore_id, task_id);
+			if (!stats || !tot) {
 				if (input->reply) {
 					char buf[128];
 					snprintf(buf, sizeof(buf),
-						 "%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%u,%u\n",
-						 lat_min_usec,
-						 lat_max_usec,
-						 lat_avg_usec,
-						 tot_lat_min_usec,
-						 tot_lat_max_usec,
-						 last_tsc,
-						 rte_get_tsc_hz(),
-						 lcore_id,
-						 task_id);
+						 "error: core %u task %u stats = %p tot = %p\n",
+						 lcore_id, task_id, stats, tot);
 					input->reply(input, buf, strlen(buf));
+				} else {
+					plog_info("error: core %u task %u stats = %p tot = %p\n",
+						  lcore_id, task_id, stats, tot);
 				}
-				else {
-					plog_info("core: %u, task: %u, min: %"PRIu64", max: %"PRIu64", avg: %"PRIu64", min since reset: %"PRIu64", max since reset: %"PRIu64"\n",
-						  lcore_id,
-						  task_id,
-						  lat_min_usec,
-						  lat_max_usec,
-						  lat_avg_usec,
-						  tot_lat_min_usec,
-						  tot_lat_max_usec);
-				}
+				continue;
+			}
+
+			uint64_t last_tsc = stats_core_task_last_tsc(lcore_id, task_id);
+			uint64_t lat_min_usec = time_unit_to_usec(&stats->min.time);
+			uint64_t lat_max_usec = time_unit_to_usec(&stats->max.time);
+			uint64_t tot_lat_min_usec = time_unit_to_usec(&tot->min.time);
+			uint64_t tot_lat_max_usec = time_unit_to_usec(&tot->max.time);
+			uint64_t lat_avg_usec = time_unit_to_usec(&stats->avg.time);
+
+			if (input->reply) {
+				char buf[128];
+				snprintf(buf, sizeof(buf),
+					 "%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%"PRIu64",%u,%u\n",
+					 lat_min_usec,
+					 lat_max_usec,
+					 lat_avg_usec,
+					 tot_lat_min_usec,
+					 tot_lat_max_usec,
+					 last_tsc,
+					 rte_get_tsc_hz(),
+					 lcore_id,
+					 task_id);
+				input->reply(input, buf, strlen(buf));
+			}
+			else {
+				plog_info("core: %u, task: %u, min: %"PRIu64", max: %"PRIu64", avg: %"PRIu64", min since reset: %"PRIu64", max since reset: %"PRIu64"\n",
+					  lcore_id,
+					  task_id,
+					  lat_min_usec,
+					  lat_max_usec,
+					  lat_avg_usec,
+					  tot_lat_min_usec,
+					  tot_lat_max_usec);
 			}
 		}
 	}