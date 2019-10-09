opnfv/samplevnf

commit fb0c44a8e62de7cf06a1ad0c025ac7ecb39cad27

Support for recent GCC compiler
This is required for instance on gcc (GCC) 8.2.1 20180905

Signed-off-by: Xavier Simonart <xavier.simonart@intel.com>
Change-Id: Id86de5d39d77c5cbf168cc51434f436f84376a4c

VNFs/DPPD-PROX/cmd_parser.c
@@ -52,6 +52,7 @@
 #include "handle_cgnat.h"
 #include "handle_impair.h"
 #include "rx_pkt.h"
+#include "prox_compat.h"
 
 static int core_task_is_valid(int lcore_id, int task_id)
 {
@@ -823,7 +824,7 @@ static int parse_cmd_rule_add(const char *str, struct input *input)
 		return -1;
 	char *fields[9];
 	char str_cpy[255];
-	strncpy(str_cpy, str, 255);
+	prox_strncpy(str_cpy, str, 255);
 	// example add rule command: rule add 15 0 1&0x0fff 1&0x0fff 0&0 128.0.0.0/1 128.0.0.0/1 5000-5000 5000-5000 allow
 	int ret = rte_strsplit(str_cpy, 255, fields, 9, ' ');
 	if (ret != 8) {
@@ -1491,7 +1492,7 @@ static int parse_cmd_stats(const char *str, struct input *input)
 	char *ret = ret2;
 	int list = 0;
 
-	strncpy(buf, str, sizeof(buf) - 1);
+	prox_strncpy(buf, str, sizeof(buf) - 1);
 	char *tok;
 	uint64_t stat_val;
 
@@ -2157,7 +2158,7 @@ static int parse_cmd_help(const char *str, struct input *input)
 
 		if (strlen(cmd_strings[i].args)) {
 			char tmp[256] = {0};
-			strncpy(tmp, cmd_strings[i].args, 128);
+			prox_strncpy(tmp, cmd_strings[i].args, 128);
 			snprintf(buf + strlen(buf), sizeof(buf) - strlen(buf), "Arguments: %s\n", tmp);
 			len2 = len;
 			if (strlen(cmd_strings[i].help)) {
@@ -2189,7 +2190,7 @@ static int parse_cmd_help(const char *str, struct input *input)
 						len3 = max_len;
 				}
 
-				strncpy(tmp, h, len3);
+				prox_strncpy(tmp, h, len3);
 				h += len3;
 				while (h[0] == ' ' && strlen(h))
 					h++;

VNFs/DPPD-PROX/cfgfile.c
@@ -26,6 +26,7 @@
 #include "parse_utils.h"
 #include "log.h"
 #include "quit.h"
+#include "prox_compat.h"
 
 #define UINT32_MAX_STR "4294967295"
 
@@ -195,7 +196,7 @@ static struct cfg_section *cfg_check_section(char *buffer, struct cfg_section *p
 		if (parse_vars(val, sizeof(val), pend))
 			return NULL;
 	} else
-		strncpy(val, pend, sizeof(val));
+		prox_strncpy(val, pend, sizeof(val));
 
 	for (len = 0; val[len] != '\0'; ++len) {
 		if (strchr(valid, val[len]) == NULL) {
@@ -275,7 +276,7 @@ int cfg_parse(struct cfg_file *pcfg, struct cfg_section *psec)
 				ret = fgets(buffer, sizeof(buffer), pcfg->pfile);
 				if (ret && *ret != '[') {
 					size_t l = strlen(buffer);
-					strncpy(lines, buffer, max_len);
+					prox_strncpy(lines, buffer, max_len);
 					max_len -= l;
 					lines += l;
 				}
@@ -299,7 +300,7 @@ int cfg_parse(struct cfg_file *pcfg, struct cfg_section *psec)
 		}
 
 		while (cfg_get_line(pcfg, buffer, MAX_CFG_STRING_LEN, psec->raw_lines) > 0) {
-			strncpy(pcfg->cur_line, buffer, sizeof(pcfg->cur_line));
+			prox_strncpy(pcfg->cur_line, buffer, sizeof(pcfg->cur_line));
 			if (*buffer == '[') {
 				if (index_count + 1 < psec->nbindex) {
 					// Need to loop - go back to recorded postion in file

VNFs/DPPD-PROX/clock.c
@@ -20,6 +20,7 @@
 #include <string.h>
 
 #include <rte_cycles.h>
+#include "prox_compat.h"
 
 /* Calibrate TSC overhead by reading NB_READ times and take the smallest value.
    Bigger values are caused by external influence and can be discarded. The best
@@ -92,7 +93,7 @@ uint64_t str_to_tsc(const char *from)
 	uint64_t ret;
 	char str[16];
 
-	strncpy(str, from, sizeof(str));
+	prox_strncpy(str, from, sizeof(str));
 
 	char *frac = strchr(str, '.');
 

VNFs/DPPD-PROX/file_utils.c
@@ -22,6 +22,7 @@
 
 #include "prox_args.h"
 #include "file_utils.h"
+#include "prox_compat.h"
 
 static char file_error_string[128] = {0};
 
@@ -44,7 +45,7 @@ static void resolve_path_cfg_dir(char *file_name, size_t len, const char *path)
 	if (path[0] != '/')
 		snprintf(file_name, len, "%s/%s", get_cfg_dir(), path);
 	else
-		strncpy(file_name, path, len);
+		prox_strncpy(file_name, path, len);
 }
 
 long file_get_size(const char *path)

VNFs/DPPD-PROX/handle_acl.c
@@ -108,6 +108,7 @@ static int handle_acl_bulk(struct task_base *tbase, struct rte_mbuf **mbufs, uin
 			break;
 		case ACL_ALLOW:
 			out[i] = 0;
+			 __attribute__ ((fallthrough));
 		case ACL_RATE_LIMIT:
 			set_tc(mbufs[i], 3);
 			break;

VNFs/DPPD-PROX/handle_dump.c
@@ -24,13 +24,14 @@
 #include "task_init.h"
 #include "task_base.h"
 #include "stats.h"
+#include "prox_compat.h"
 
 struct task_dump {
 	struct task_base base;
 	uint32_t n_mbufs;
         struct rte_mbuf **mbufs;
 	uint32_t n_pkts;
-	char pcap_file[128];
+	char pcap_file[256];
 };
 
 static uint16_t buffer_packets(struct task_dump *task, struct rte_mbuf **mbufs, uint16_t n_pkts)
@@ -69,7 +70,7 @@ static void init_task_dump(struct task_base *tbase, __attribute__((unused)) stru
 	if (!strcmp(targ->pcap_file, "")) {
 		strcpy(targ->pcap_file, "out.pcap");
 	}
-	strncpy(task->pcap_file, targ->pcap_file, sizeof(task->pcap_file));
+	prox_strncpy(task->pcap_file, targ->pcap_file, sizeof(task->pcap_file));
 }
 
 static void stop(struct task_base *tbase)

VNFs/DPPD-PROX/log.c
@@ -29,6 +29,7 @@
 #include "defaults.h"
 #include "etypes.h"
 #include "prox_cfg.h"
+#include "prox_compat.h"
 
 static pthread_mutex_t file_mtx = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
 int log_lvl = PROX_MAX_LOG_LVL;
@@ -56,7 +57,7 @@ const char *get_warning(int i)
 
 static void store_warning(const char *warning)
 {
-	strncpy(last_warn[n_warnings % 5], warning, sizeof(last_warn[0]));
+	prox_strncpy(last_warn[n_warnings % 5], warning, sizeof(last_warn[0]));
 	n_warnings++;
 }
 
@@ -69,10 +70,10 @@ void plog_init(const char *log_name, int log_name_pid)
 		if (log_name_pid)
 			snprintf(buf, sizeof(buf), "%s-%u.log", "prox", getpid());
 		else
-			strncpy(buf, "prox.log", sizeof(buf));
+			prox_strncpy(buf, "prox.log", sizeof(buf));
 	}
 	else {
-		strncpy(buf, log_name, sizeof(buf));
+		prox_strncpy(buf, log_name, sizeof(buf));
 	}
 
 	fp = fopen(buf, "w");

VNFs/DPPD-PROX/parse_utils.c
@@ -34,6 +34,7 @@
 #include "log.h"
 #include "prox_lua.h"
 #include "prox_lua_types.h"
+#include "prox_compat.h"
 
 #define MAX_NB_PORT_NAMES PROX_MAX_PORTS
 #define MAX_LEN_PORT_NAME 24
@@ -117,7 +118,7 @@ int parse_single_var(char *val, size_t len, const char *name)
 				 match->name, match->val);
 			return -1;
 		}
-		strncpy(val, match->val, len);
+		prox_strncpy(val, match->val, len);
 		return 0;
 	}
 	else {
@@ -174,7 +175,7 @@ int parse_vars(char *val, size_t len, const char *name)
 					return -1;
 				}
 
-				strncpy(&cur_var[1], &name[start_var], var_len);
+				prox_strncpy(&cur_var[1], &name[start_var], var_len);
 				cur_var[1 + var_len] = 0;
 				if (parse_single_var(parsed, sizeof(parsed), cur_var)) {
 					return -1;
@@ -195,7 +196,7 @@ int parse_vars(char *val, size_t len, const char *name)
 			break;
 		}
 	}
-	strncpy(val, result, len);
+	prox_strncpy(val, result, len);
 
 	return 0;
 }
@@ -922,10 +923,12 @@ int parse_kmg(uint32_t* val, const char *str2)
 		if (*val >> 22)
 			return -2;
 		*val <<= 10;
+		 __attribute__ ((fallthrough));
 	case 'M':
 		if (*val >> 22)
 			return -2;
 		*val <<= 10;
+		 __attribute__ ((fallthrough));
 	case 'K':
 		if (*val >> 22)
 			return -2;
@@ -1048,7 +1051,7 @@ int parse_str(char* dst, const char *str2, size_t max_len)
 		return -2;
 	}
 
-	strncpy(dst, str, max_len);
+	prox_strncpy(dst, str, max_len);
 	return 0;
 }
 
@@ -1122,7 +1125,7 @@ int parse_remap(uint8_t *mapping, const char *str)
 		set_errf("String too long (max supported: %d)", MAX_STR_LEN_PROC);
 		return -2;
 	}
-	strncpy(str_cpy, str, MAX_STR_LEN_PROC);
+	prox_strncpy(str_cpy, str, MAX_STR_LEN_PROC);
 
 	ret = rte_strsplit(str_cpy, strlen(str_cpy), elements, PROX_MAX_PORTS + 1, ',');
 	if (ret <= 0) {
@@ -1179,7 +1182,7 @@ int add_port_name(uint32_t val, const char *str2)
 	}
 
 	pn = &port_names[nb_port_names];
-	strncpy(pn->name, str, sizeof(pn->name));
+	prox_strncpy(pn->name, str, sizeof(pn->name));
 	pn->id = val;
 
 	++nb_port_names;
@@ -1197,7 +1200,7 @@ int set_self_var(const char *str)
 
 	struct var *v = &vars[nb_vars];
 
-	strncpy(v->name, "$self", strlen("$self"));
+	prox_strncpy(v->name, "$self", strlen("$self") + 1);
 	sprintf(v->val, "%s", str);
 	nb_vars++;
 
@@ -1245,8 +1248,8 @@ int add_var(const char* name, const char *str2, uint8_t cli)
 	v = &vars[nb_vars];
 	PROX_PANIC(strlen(name) > sizeof(v->name), "\tUnable to parse var %s: too long\n", name);
 	PROX_PANIC(strlen(str) > sizeof(v->val), "\tUnable to parse var %s=%s: too long\n", name,str);
-	strncpy(v->name, name, sizeof(v->name));
-	strncpy(v->val, str, sizeof(v->val));
+	prox_strncpy(v->name, name, sizeof(v->name));
+	prox_strncpy(v->val, str, sizeof(v->val));
 	v->cli = cli;
 
 	++nb_vars;

commit da4e41a67cca12cc4160a66ca46a37889137fa1e

Fix strncpy issue introduced by fb0c44a8
Change-Id: I833d753bd5c426d50c0c9fdee5668f3b9fff13b9
Signed-off-by: Xavier Simonart <xavier.simonart@intel.com>

VNFs/DPPD-PROX/cmd_parser.c
@@ -2272,7 +2272,10 @@ static int parse_cmd_help(const char *str, struct input *input)
 						len3 = max_len;
 				}
 
-				prox_strncpy(tmp, h, len3);
+				// Use strncpy here and not prox_strncpy. The dest (tmp) has been initialized with 0.
+				// The fact that we are copying 80 characters potentially not null terminated is hence not an issue.
+				// Using prox_strncpy here might cause a PROX_PANIC
+				strncpy(tmp, h, len3);
 				h += len3;
 				while (h[0] == ' ' && strlen(h))
 					h++;