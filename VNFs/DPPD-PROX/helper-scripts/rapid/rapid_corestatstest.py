0c4751e432a42fd167e2a76b40958ef2ff682498
Luc Provoost <luc.provoost@intel.com>
2020-05-29 13:47:03 +0200

    Fixing some more copyright dates
    New files created in 2020 have now the right date in the copyright
    notice.
    
    Change-Id: I8eb244beec3d793e819cce1f63c39671f18286a1
    Signed-off-by: Luc Provoost <luc.provoost@intel.com>

--- VNFs/DPPD-PROX/helper-scripts/rapid/rapid_corestatstest.py
+++ VNFs/DPPD-PROX/helper-scripts/rapid/rapid_corestatstest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ##
-## Copyright (c) 2010-2020 Intel Corporation
+## Copyright (c) 2020 Intel Corporation
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
