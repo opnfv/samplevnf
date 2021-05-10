894f62216ca3c97ef0c1c09b9475459b9324b1bb
Luc Provoost <luc.provoost@intel.com>
2021-05-10 10:06:30 +0200

    Reinstate installation of igb_uio
    For good performance on some platforms, we need the igb_uio driver. With
    this change, the driver is compiled and insmod done during boot time.
    When running the rapid scripts, the devbind.sh script is sent to the
    instances (after replacing the string "MACADDRESS"). Make sure you
    uncomment the proper line to chose which driver you want to use.
    
    Change-Id: I69549400a97f29e06add6ab44515e96ffa9ce0e8
    Signed-off-by: Luc Provoost <luc.provoost@intel.com>

--- VNFs/DPPD-PROX/helper-scripts/rapid/devbind.sh
+++ VNFs/DPPD-PROX/helper-scripts/rapid/devbind.sh
@@ -2,9 +2,11 @@ link="$(sudo ip -o link | grep MACADDRESS |cut -d":" -f 2)"
 if [ -n "$link" ];
 then
        echo Need to bind
+       # Uncomment one of the following lines, depending on which driver
+       # you want to use: vfio-pci or igb_uio
        #sudo /opt/rapid/dpdk/usertools/dpdk-devbind.py --force --bind igb_uio $(sudo /opt/rapid/dpdk/usertools/dpdk-devbind.py --status |grep  $link | cut -d" " -f 1)
        sudo driverctl set-override $(sudo ethtool -i $link |grep bus-info | cut -d" " -f 2) vfio-pci
 else
-       echo Assuming port is already bound to vfio-pci
+       echo Assuming port is already bound to DPDK poll mode driver
 fi
 exit 0
