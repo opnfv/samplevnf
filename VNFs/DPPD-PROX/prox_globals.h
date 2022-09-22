opnfv/samplevnf

commit 9a1e1e9336b39178362c6ccadd4a9716f865047f

Added support for multiple vlans in ipv4 vdev mode
When the following conditions are met:
- ipv4
- vdev enabled
Then multiple vlans are supported.
Multiple vlans can be configured using the vlan_tag option within the port section of the config file.
Multiple (different) local_ipv4/prefix must then also be configured within the same section.

Note that for ipv6 multiple vlans are not supported (yet)
Also, when vdev is not enabled, multiple vlans are also not (yet) supported.

Change-Id: I38b6cd98ff2a0f484d1bf0910b15413ba21ae6d6
Signed-off-by: Xavier Simonart <xavier.simonart@intel.com>

VNFs/DPPD-PROX/prox_port_cfg.c
@@ -214,6 +214,12 @@ void init_rte_dev(int use_dummy_devices)
 			continue;
 		}
 		struct prox_port_cfg* port_cfg = &prox_port_cfg[port_id];
+
+		prox_port_cfg[port_id].n_vlans = 0;
+		while ((prox_port_cfg[port_id].n_vlans < PROX_MAX_VLAN_TAGS) && (prox_port_cfg[port_id].vlan_tags[prox_port_cfg[port_id].n_vlans])) {
+			prox_port_cfg[port_id].n_vlans++;
+		}
+
 		if (port_cfg->vdev[0]) {
 			char name[MAX_NAME_SIZE], tap[MAX_NAME_SIZE];
 			snprintf(tap, MAX_NAME_SIZE, "net_tap%d", port_id);
@@ -231,20 +237,30 @@ void init_rte_dev(int use_dummy_devices)
 			prox_port_cfg[vdev_port_id].active = 1;
 			prox_port_cfg[vdev_port_id].dpdk_mapping = port_id;
 			prox_port_cfg[vdev_port_id].n_txq = 1;
+			prox_port_cfg[vdev_port_id].n_vlans = prox_port_cfg[port_id].n_vlans;
 
-			if (prox_port_cfg[port_id].vlan_tag) {
+			for (uint32_t tag_id = 0; tag_id < prox_port_cfg[port_id].n_vlans; tag_id++) {
+				prox_port_cfg[vdev_port_id].vlan_tags[tag_id] = prox_port_cfg[port_id].vlan_tags[tag_id];
 				char command[1024];
-				snprintf(prox_port_cfg[vdev_port_id].name, MAX_NAME_SIZE, "%s_%d", port_cfg->vdev, prox_port_cfg[port_id].vlan_tag);
-				sprintf(command, "ip link add link %s name %s type vlan id %d", port_cfg->vdev, prox_port_cfg[vdev_port_id].name, prox_port_cfg[port_id].vlan_tag);
+				snprintf(prox_port_cfg[vdev_port_id].names[tag_id], MAX_NAME_SIZE, "%s_%d", port_cfg->vdev, prox_port_cfg[port_id].vlan_tags[tag_id]);
+				sprintf(command, "ip link add link %s name %s type vlan id %d", port_cfg->vdev, prox_port_cfg[vdev_port_id].names[tag_id], prox_port_cfg[port_id].vlan_tags[tag_id]);
 				system(command);
-				plog_info("Running %s\n", command);
-				plog_info("Using vlan tag %d - added device %s\n", prox_port_cfg[port_id].vlan_tag, prox_port_cfg[vdev_port_id].name);
-			} else
-				strncpy(prox_port_cfg[vdev_port_id].name, port_cfg->vdev, MAX_NAME_SIZE);
+				plog_info("\tRunning %s\n", command);
+				plog_info("\tUsing vlan tag %d - added device %s\n", prox_port_cfg[port_id].vlan_tags[tag_id], prox_port_cfg[vdev_port_id].names[tag_id]);
+			}
+			if (prox_port_cfg[port_id].n_vlans == 0) {
+				strncpy(prox_port_cfg[vdev_port_id].names[0], port_cfg->vdev, MAX_NAME_SIZE);
+				prox_port_cfg[vdev_port_id].n_vlans = 1;
+				prox_port_cfg[vdev_port_id].vlan_tags[0] = 0;
+			}
 
 			prox_port_cfg[port_id].dpdk_mapping = vdev_port_id;
-			prox_port_cfg[vdev_port_id].ip = rte_be_to_cpu_32(prox_port_cfg[port_id].ip);
-			prox_port_cfg[vdev_port_id].prefix = prox_port_cfg[port_id].prefix;
+			uint32_t i = 0;
+			while ((i < PROX_MAX_VLAN_TAGS) && (prox_port_cfg[port_id].ip_addr[i].ip)) {
+				prox_port_cfg[vdev_port_id].ip_addr[i].ip = rte_be_to_cpu_32(prox_port_cfg[port_id].ip_addr[i].ip);
+				prox_port_cfg[vdev_port_id].ip_addr[i].prefix = prox_port_cfg[port_id].ip_addr[i].prefix;
+				i++;
+			}
 			prox_port_cfg[vdev_port_id].type = prox_port_cfg[port_id].type;
 			if (prox_port_cfg[vdev_port_id].type == PROX_PORT_MAC_HW) {
 				// If DPDK port MAC set to HW, then make sure the vdev has the same MAC as DPDK port
@@ -254,6 +270,10 @@ void init_rte_dev(int use_dummy_devices)
 					vdev_port_id, port_id, MAC_BYTES(prox_port_cfg[vdev_port_id].eth_addr.addr_bytes));
 			} else
 				memcpy(&prox_port_cfg[vdev_port_id].eth_addr, &prox_port_cfg[port_id].eth_addr, sizeof(prox_port_cfg[port_id].eth_addr));
+		}
+		if (prox_port_cfg[port_id].n_vlans == 0) {
+			prox_port_cfg[port_id].n_vlans = 1;
+			prox_port_cfg[port_id].vlan_tags[0] = 0;
 		}
 	}
 	nb_ports = prox_rte_eth_dev_count_avail();
@@ -425,7 +445,7 @@ uint8_t init_rte_ring_dev(void)
 			struct rte_ring* tx_ring = rte_ring_lookup(port_cfg->tx_ring);
 			PROX_PANIC(tx_ring == NULL, "Ring %s not found for port %d!\n", port_cfg->tx_ring, port_id);
 
-			int ret = rte_eth_from_rings(port_cfg->name, &rx_ring, 1, &tx_ring, 1, rte_socket_id());
+			int ret = rte_eth_from_rings(port_cfg->names[0], &rx_ring, 1, &tx_ring, 1, rte_socket_id());
 			PROX_PANIC(ret != 0, "Failed to create eth_dev from rings for port %d\n", port_id);
 
 			port_cfg->port_conf.intr_conf.lsc = 0; /* Link state interrupt not supported for ring-backed ports */
@@ -443,7 +463,7 @@ static void print_port_capa(struct prox_port_cfg *port_cfg)
 
 	port_id = port_cfg - prox_port_cfg;
 	plog_info("\t*** Initializing port %u ***\n", port_id);
-	plog_info("\t\tPort name is set to %s\n", port_cfg->name);
+	plog_info("\t\tPort name is set to %s\n", port_cfg->names[0]);
 	plog_info("\t\tPort max RX/TX queue is %u/%u\n", port_cfg->max_rxq, port_cfg->max_txq);
 	plog_info("\t\tPort driver is %s\n", port_cfg->driver_name);
 #if RTE_VERSION >= RTE_VERSION_NUM(16,4,0,0)
@@ -802,8 +822,10 @@ static void init_port(struct prox_port_cfg *port_cfg)
 	PROX_PANIC(ret < 0, "\n\t\t\trte_eth_dev_start() failed on port %u: error %d\n", port_id, ret);
 	plog_info(" done: ");
 
-	if ((prox_port_cfg[port_id].ip) && (prox_port_cfg[port_id].is_vdev)) {
-		set_ip_address(prox_port_cfg[port_id].name, &prox_port_cfg[port_id].ip, prox_port_cfg[port_id].prefix);
+	if (prox_port_cfg[port_id].is_vdev) {
+		for (int vlan_id = 0; vlan_id < prox_port_cfg[port_id].n_vlans; vlan_id++) {
+			set_ip_address(prox_port_cfg[port_id].names[vlan_id], &prox_port_cfg[port_id].ip_addr[vlan_id].ip, prox_port_cfg[port_id].ip_addr[vlan_id].prefix);
+		}
 	}
 	/* Getting link status can be done without waiting if Link
 	   State Interrupt is enabled since in that case, if the link

VNFs/DPPD-PROX/prox_port_cfg.h
@@ -28,12 +28,14 @@
 
 #include "prox_compat.h"
 #include "prox_globals.h"
+#include "ip_subnet.h"
 
 enum addr_type {PROX_PORT_MAC_HW, PROX_PORT_MAC_SET, PROX_PORT_MAC_RAND};
 
 #define IPV4_CKSUM	1
 #define UDP_CKSUM	2
 #define NB_MCAST_ADDR	16
+#define PROX_MAX_VLAN_TAGS	256
 
 struct prox_port_cfg {
 	struct rte_mempool *pool[32];  /* Rx/Tx mempool */
@@ -55,7 +57,7 @@ struct prox_port_cfg {
 	uint32_t  mtu;
 	enum addr_type    type;
 	prox_rte_ether_addr eth_addr;    /* port MAC address */
-	char name[MAX_NAME_SIZE];
+	char names[PROX_MAX_VLAN_TAGS][MAX_NAME_SIZE];
 	char vdev[MAX_NAME_SIZE];
 	char short_name[MAX_NAME_SIZE];
 	char driver_name[MAX_NAME_SIZE];
@@ -82,12 +84,12 @@ struct prox_port_cfg {
 	uint8_t available;
 	prox_rte_ether_addr mc_addr[NB_MCAST_ADDR];
 	int dpdk_mapping;
-	uint32_t ip;
-	int fd;
-	uint32_t vlan_tag;
-	uint8_t prefix;
+	struct ip4_subnet ip_addr[PROX_MAX_VLAN_TAGS];
+	int fds[PROX_MAX_VLAN_TAGS];
+	uint32_t vlan_tags[PROX_MAX_VLAN_TAGS];
 	uint8_t is_vdev;
-    uint8_t all_rx_queues;
+	uint8_t all_rx_queues;
+	uint16_t n_vlans;
 };
 
 extern rte_atomic32_t lsc;

VNFs/DPPD-PROX/cmd_parser.c
@@ -995,12 +995,7 @@ static int parse_cmd_local_ip(const char *str, struct input *input)
 		struct task_base *tbase = lcore_cfg[lcore_id].tasks_all[task_id];
 		uint32_t local_ip = ((ip[3] & 0xFF) << 24) | ((ip[2] & 0xFF) << 16) | ((ip[1] & 0xFF) << 8) | ((ip[0] & 0xFF) << 0);
 		if (!task_is_mode_and_submode(lcore_id, task_id, "arp", "local")) {
-			if (!task_is_sub_mode(lcore_id, task_id, "l3")) {
-				plog_err("Core %u task %u is not in l3 mode\n", lcore_id, task_id);
-			} else {
-				plog_info("Setting local ip to %s\n", str);
-				task_set_local_ip(tbase, local_ip);
-			}
+			plog_err("Core %u task %u is not in arp mode\n", lcore_id, task_id);
 		} else {
 			plog_info("Setting local ip to %s\n", str);
 			task_arp_set_local_ip(tbase, local_ip);

VNFs/DPPD-PROX/commands.c
@@ -895,7 +895,7 @@ void cmd_portinfo(int port_id, char *dst, size_t max_len)
 			dst += snprintf(dst, end - dst,
 					"%2d:%10s; "MAC_BYTES_FMT"; %s\n",
 					port_id,
-					port_cfg->name,
+					port_cfg->names[0],
 					MAC_BYTES(port_cfg->eth_addr.addr_bytes),
 					port_cfg->pci_addr);
 		}
@@ -909,7 +909,7 @@ void cmd_portinfo(int port_id, char *dst, size_t max_len)
 	struct prox_port_cfg* port_cfg = &prox_port_cfg[port_id];
 
 	dst += snprintf(dst, end - dst, "Port info for port %u\n", port_id);
-	dst += snprintf(dst, end - dst, "\tName: %s\n", port_cfg->name);
+	dst += snprintf(dst, end - dst, "\tName: %s\n", port_cfg->names[0]);
 	dst += snprintf(dst, end - dst, "\tDriver: %s\n", port_cfg->driver_name);
 	dst += snprintf(dst, end - dst, "\tMac address: "MAC_BYTES_FMT"\n", MAC_BYTES(port_cfg->eth_addr.addr_bytes));
 	dst += snprintf(dst, end - dst, "\tLink speed: %u Mbps\n", port_cfg->link_speed);

VNFs/DPPD-PROX/display_pkt_len.c
@@ -81,7 +81,7 @@ static void display_pkt_len_draw_frame(struct screen_state *screen_state)
 		const uint32_t port_id = port_disp[i];
 
 		display_column_print(port_col, i, "%4u", port_id);
-		display_column_print(name_col, i, "%8s", prox_port_cfg[port_id].name);
+		display_column_print(name_col, i, "%8s", prox_port_cfg[port_id].names[0]);
 		display_column_print(type_col, i, "%7s", prox_port_cfg[port_id].short_name);
 	}
 }

VNFs/DPPD-PROX/display_ports.c
@@ -116,7 +116,7 @@ static void display_ports_draw_frame(struct screen_state *state)
 		const uint32_t port_id = port_disp[i];
 
 		display_column_print(nb_col, i, "%u", port_id);
-		display_column_print(name_col, i, "%s", prox_port_cfg[port_id].name);
+		display_column_print(name_col, i, "%s", prox_port_cfg[port_id].names[0]);
 		display_column_print(type_col, i, "%s", prox_port_cfg[port_id].short_name);
 	}
 }

VNFs/DPPD-PROX/display_rings.c
@@ -68,7 +68,7 @@ static void display_rings_draw_frame(struct screen_state *state)
 			int offset = 0;
 
 			for (uint32_t j = 0; j < rs->nb_ports; j++)
-				offset += sprintf(name + offset, "%s", rs->port[j]->name);
+				offset += sprintf(name + offset, "%s", rs->port[j]->names[0]);
 		}
 
 		sc_val = (rs->ring->flags & RING_F_SC_DEQ) ? 'y' : 'n';

VNFs/DPPD-PROX/handle_master.c
@@ -167,16 +167,17 @@ void master_init_vdev(struct task_base *tbase, uint8_t port_id, uint8_t core_id,
 
 		struct sockaddr_in dst, src;
 		src.sin_family = AF_INET;
-		src.sin_addr.s_addr = prox_port_cfg[vdev_port].ip;
 		src.sin_port = rte_cpu_to_be_16(PROX_PSEUDO_PKT_PORT);
-
-		int fd = socket(AF_INET,  SOCK_DGRAM, 0);
-		PROX_PANIC(fd < 0, "Failed to open socket(AF_INET,  SOCK_DGRAM, 0)\n");
-		prox_port_cfg[vdev_port].fd = fd;
-		rc = bind(fd,(struct sockaddr *)&src, sizeof(struct sockaddr_in));
-		PROX_PANIC(rc, "Failed to bind("IPv4_BYTES_FMT":%d): errno = %d (%s)\n", IPv4_BYTES(((uint8_t*)&src.sin_addr.s_addr)), src.sin_port, errno, strerror(errno));
-		plog_info("DPDK port %d bound("IPv4_BYTES_FMT":%d) to fd %d\n", port_id, IPv4_BYTES(((uint8_t*)&src.sin_addr.s_addr)), src.sin_port, fd);
-		fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+		for (int vlan_id = 0; vlan_id < prox_port_cfg[vdev_port].n_vlans; vlan_id++) {
+			src.sin_addr.s_addr = prox_port_cfg[vdev_port].ip_addr[vlan_id].ip;
+			int fd = socket(AF_INET,  SOCK_DGRAM, 0);
+			PROX_PANIC(fd < 0, "Failed to open socket(AF_INET,  SOCK_DGRAM, 0)\n");
+			prox_port_cfg[vdev_port].fds[vlan_id] = fd;
+			rc = bind(fd,(struct sockaddr *)&src, sizeof(struct sockaddr_in));
+			PROX_PANIC(rc, "Failed to bind("IPv4_BYTES_FMT":%d): errno = %d (%s)\n", IPv4_BYTES(((uint8_t*)&src.sin_addr.s_addr)), src.sin_port, errno, strerror(errno));
+			plog_info("DPDK port %d bound("IPv4_BYTES_FMT":%d) to fd %d\n", port_id, IPv4_BYTES(((uint8_t*)&src.sin_addr.s_addr)), src.sin_port, fd);
+			fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+		}
 		task->max_vdev_id++;
 	}
 }
@@ -783,6 +784,7 @@ static inline void handle_message(struct task_base *tbase, struct rte_mbuf *mbuf
 
 			struct ether_hdr_arp *hdr_arp = rte_pktmbuf_mtod(mbuf, struct ether_hdr_arp *);
 			uint32_t ip = get_ip(mbuf);
+			vlan = ctrl_ring_get_vlan(mbuf);
 			struct rte_ring *ring = task->ctrl_tx_rings[get_core(mbuf) * MAX_TASKS_PER_CORE + get_task(mbuf)];
 
 			// First check whether MAC address is not already in kernel MAC table.
@@ -803,12 +805,23 @@ static inline void handle_message(struct task_base *tbase, struct rte_mbuf *mbuf
 			dst.sin_family = AF_INET;
 			dst.sin_addr.s_addr = ip;
 			dst.sin_port = rte_cpu_to_be_16(PROX_PSEUDO_PKT_PORT);
-			// TODO VLAN: find the right fd based on the VLAN
-			int n = sendto(prox_port_cfg[vdev_port].fd, (char*)(&ip), 0, MSG_DONTROUTE,  (struct sockaddr *)&dst, sizeof(struct sockaddr_in));
+
+			int vlan_id;
+			for (vlan_id = 0; vlan_id < prox_port_cfg[vdev_port].n_vlans; vlan_id++) {
+				if (prox_port_cfg[vdev_port].vlan_tags[vlan_id] == vlan)
+					break;
+			}
+			if (vlan_id >= prox_port_cfg[vdev_port].n_vlans) {
+				// Tag not found
+				plogx_info("\tDid not send to TAP IP "IPv4_BYTES_FMT" as wrong VLAN %d\n", IPv4_BYTES(((uint8_t*)&ip)), vlan);
+				tx_drop(mbuf);
+				break;
+			}
+			int n = sendto(prox_port_cfg[vdev_port].fds[vlan_id], (char*)(&ip), 0, MSG_DONTROUTE,  (struct sockaddr *)&dst, sizeof(struct sockaddr_in));
 			if (n < 0) {
-				plogx_info("\tFailed to send to TAP IP "IPv4_BYTES_FMT" using fd %d, error = %d (%s)\n", IPv4_BYTES(((uint8_t*)&ip)), prox_port_cfg[vdev_port].fd, errno, strerror(errno));
+				plogx_info("\tFailed to send to TAP IP "IPv4_BYTES_FMT" using fd %d, error = %d (%s)\n", IPv4_BYTES(((uint8_t*)&ip)), prox_port_cfg[vdev_port].fds[vlan_id], errno, strerror(errno));
 			} else
-				plogx_dbg("\tSent %d bytes to TAP IP "IPv4_BYTES_FMT" using fd %d\n", n, IPv4_BYTES(((uint8_t*)&ip)), prox_port_cfg[vdev_port].fd);
+				plogx_dbg("\tSent %d bytes to TAP IP "IPv4_BYTES_FMT" using fd %d\n", n, IPv4_BYTES(((uint8_t*)&ip)), prox_port_cfg[vdev_port].fds[vlan_id]);
 
 			record_request(tbase, ip, port, ring);
 			tx_drop(mbuf);
@@ -1028,8 +1041,14 @@ static void handle_route_event(struct task_base *tbase)
 	}
 	int dpdk_vdev_port = -1;
 	for (int i = 0; i< prox_rte_eth_dev_count_avail(); i++) {
-		if (strcmp(prox_port_cfg[i].name, interface_name) == 0)
-			dpdk_vdev_port = i;
+		for (int vlan_id = 0; vlan_id < prox_port_cfg[i].n_vlans; vlan_id++) {
+			if (strcmp(prox_port_cfg[i].names[vlan_id], interface_name) == 0) {
+				dpdk_vdev_port = i;
+				break;
+			}
+		}
+		if (dpdk_vdev_port != -1)
+			break;
 	}
 	if (dpdk_vdev_port != -1) {
 		plogx_info("Received netlink message on tap interface %s for IP "IPv4_BYTES_FMT"/%d, Gateway  "IPv4_BYTES_FMT"\n", interface_name, IP4(ip), dst_len, IP4(gw_ip));

commit afc294bc360455c639af23a51df3afc0133723fe

Fix name-array out-of-memory
"struct prox_port_cfg" "names" buffer size is 64 and it will lead to
out-of-memory when do "snprintf" operation. Modify code to enlarge
buffer size to 128.

Fixes: 9a1e1e9336b3 ("Added support for multiple vlans in ipv4 vdev mode")
Cc: xavier.simonart@intel.com

Signed-off-by: Long Wu <long.wu@corigine.com>
Reviewed-by: Simon Horman <simon.horman@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>
Change-Id: Iafbde578688e1a59f4b6e655e45b2d2951f36162

VNFs/DPPD-PROX/prox_globals.h
@@ -18,6 +18,7 @@
 #define MAX_TASKS_PER_CORE      8
 #define MAX_SOCKETS             64
 #define MAX_NAME_SIZE           64
+#define MAX_NAME_BUFFER_SIZE    128
 #define MAX_PROTOCOLS           3
 #define MAX_RINGS_PER_TASK      (MAX_WT_PER_LB*MAX_PROTOCOLS)
 #define MAX_WT_PER_LB           64

VNFs/DPPD-PROX/prox_port_cfg.c
@@ -227,10 +227,10 @@ void init_rte_dev(int use_dummy_devices)
 		}
 
 		if (port_cfg->vdev[0]) {
-			char name[MAX_NAME_SIZE], tap[MAX_NAME_SIZE];
+			char name[MAX_NAME_BUFFER_SIZE], tap[MAX_NAME_SIZE];
 			snprintf(tap, MAX_NAME_SIZE, "net_tap%d", port_id);
 #if (RTE_VERSION > RTE_VERSION_NUM(17,5,0,1))
-			snprintf(name, MAX_NAME_SIZE, "iface=%s", port_cfg->vdev);
+			snprintf(name, MAX_NAME_BUFFER_SIZE, "iface=%s", port_cfg->vdev);
 			rc = rte_vdev_init(tap, name);
 #else
 			PROX_PANIC(1, "vdev not supported in DPDK < 17.05\n");
@@ -248,7 +248,7 @@ void init_rte_dev(int use_dummy_devices)
 			for (uint32_t tag_id = 0; tag_id < prox_port_cfg[port_id].n_vlans; tag_id++) {
 				prox_port_cfg[vdev_port_id].vlan_tags[tag_id] = prox_port_cfg[port_id].vlan_tags[tag_id];
 				char command[1024];
-				snprintf(prox_port_cfg[vdev_port_id].names[tag_id], MAX_NAME_SIZE, "%s_%d", port_cfg->vdev, prox_port_cfg[port_id].vlan_tags[tag_id]);
+				snprintf(prox_port_cfg[vdev_port_id].names[tag_id], MAX_NAME_BUFFER_SIZE, "%s_%d", port_cfg->vdev, prox_port_cfg[port_id].vlan_tags[tag_id]);
 				sprintf(command, "ip link add link %s name %s type vlan id %d", port_cfg->vdev, prox_port_cfg[vdev_port_id].names[tag_id], prox_port_cfg[port_id].vlan_tags[tag_id]);
 				system(command);
 				plog_info("\tRunning %s\n", command);

VNFs/DPPD-PROX/prox_port_cfg.h
@@ -57,7 +57,7 @@ struct prox_port_cfg {
 	uint32_t  mtu;
 	enum addr_type    type;
 	prox_rte_ether_addr eth_addr;    /* port MAC address */
-	char names[PROX_MAX_VLAN_TAGS][MAX_NAME_SIZE];
+	char names[PROX_MAX_VLAN_TAGS][MAX_NAME_BUFFER_SIZE];
 	char vdev[MAX_NAME_SIZE];
 	char short_name[MAX_NAME_SIZE];
 	char driver_name[MAX_NAME_SIZE];