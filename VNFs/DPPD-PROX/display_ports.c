6838702b507e2e2d7b14d5bbd705d06e56da1857
Luc Provoost <luc.provoost@gmail.com>
2024-11-08 15:49:30 +0100

    fix crc offloading for iavf ports
    Apply i40e_vf CRC offload code to iavf ports
    
    Signed-off-by: Luc Provoost <luc.provoost@gmail.com>
    Change-Id: Ife50c99447ede118941d50c320038f716f08a301

--- VNFs/DPPD-PROX/display_ports.c
+++ VNFs/DPPD-PROX/display_ports.c
@@ -179,7 +179,8 @@ static void display_ports_draw_per_sec_stats(void)
 		/* Take 20 bytes overhead (or 24 if crc strip is enabled) into accound */
 		struct percent rx_percent;
 		struct percent tx_percent;
-		if (strcmp(prox_port_cfg[port_id].short_name, "i40e_vf") == 0) {
+		if ((strcmp(prox_port_cfg[port_id].short_name, "i40e_vf") == 0) ||
+				(strcmp(prox_port_cfg[port_id].short_name, "iavf") == 0)) {
 #if defined (RTE_ETH_RX_OFFLOAD_CRC_STRIP)
 			if (prox_port_cfg[port_id].requested_rx_offload & RTE_ETH_RX_OFFLOAD_CRC_STRIP) {
 				rx_percent = calc_percent(last->rx_bytes - prev->rx_bytes + 20 * (last->rx_tot - prev->rx_tot), delta_t);
